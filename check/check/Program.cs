b9fafda952d8d63aa92b8c39453b190b159d5213
mason158 <alicemason69@gmail.com>
2021-04-30 12:01:14 +0400

    commit

---
+++ check/check/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько бабла у вас?");
+
+            Console.ReadLine();
+
+            
+            Console.WriteLine("Выберите товар: \na - Хлеб: 45 копеек \nb - Молоко: 2, 35 манат \nc - Соль: 56 копеек \nd - Кола: 1, 34 манат \ne - Сладости: 5 манат \nf - Арахис: 1, 49 манат \ng - Жевачка: 73 копейки \nh - Сигареты: 3, 94 маната");
+
+            double a, b, c, d, e, f, g, h;
+            a = 0.45; b = 2.35; c = 0.56; d = 1.34; e = 5; f = 1.49; g = 0.73; h = 3.94;
+            
+            for (double i = 0; i < 14; i++)
+            {
+                i = quantity*
+                Console.WriteLine("Выберите товар:");
+                double num = double.Parse(Console.ReadLine());
+            }
+             
+        }
+    }
+}
